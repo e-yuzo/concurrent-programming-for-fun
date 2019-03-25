5d559d429d3229a8551292e50b73395d2778aba3
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2019-03-25 15:31:37 -0300

    add threads introduction

---
+++ ThreadCreation.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads.introduction;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author a1354698
+ * Description: given INT X, run X threads.
+ */
+public class ThreadCreation implements Runnable{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberOfThreads = scan.nextInt();
+        System.out.println(numberOfThreads);
+        int i = 0;
+        while (i < numberOfThreads) {
+            new Thread(new ThreadCreation()).start();
+            i++;
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("woooo wo wo wooo wooooo: ghostspeak amulet needed.");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadCreation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ ThreadSafety.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads.introduction;
+
+/**
+ *
+ * @author a1354698
+ * Description: thread safety.
+ */
+public class ThreadSafety {
+    
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        new Thread(new SharedObject(counter)).start();
+        new Thread(new SharedObject(counter)).start();
+        new Thread(new SharedObject(counter)).start();
+    }
+}
+
+class SharedObject implements Runnable {
+
+    Counter counter;
+
+    public SharedObject(Counter counter) {
+        this.counter = counter;
+    }      
+        
+    private void bug() {
+        counter.add();
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            bug();
+            System.out.println(counter.getValue());
+        }
+    }
+}
+
+class Counter {
+    
+    private int value;
+    
+    public Counter() {
+        this.value = 0;
+    }
+    
+    public void add() {
+        this.value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+
