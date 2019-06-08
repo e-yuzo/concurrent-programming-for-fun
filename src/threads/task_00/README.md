03967865385fc7d163f03f4d579dfef3e41a8964
yuzo <yuzo@pop-os.localdomain>
2019-06-08 14:14:29 -0300

    fixing random stuff

--- src/threads/task_00/README.md
+++
@@ -1,6 +0,0 @@
-# Concurrent programming exercises using Java.
-Organização dos exercícios realizados na disciplina de Programação Concorrente.
-
-- Aluno: Eduardo Yuzo Nakai.
-- Professor: Rodrigo Campiolo.
-- Ano e semestre: 2019, 1°.
--- src/threads/task_01/ex_02/ThreadReader.java
+++ src/threads/task_01/ex_02/ThreadReader.java
@@ -26,7 +26,7 @@ public class ThreadReader implements Runnable {
     public void run() {
         while (true) {
             String fileData;
-            fileData = readFile("/home/yuzo/NetBeansProjects/ConcurrentProgramming/src/threads/task_01/quotes.txt");
+            fileData = readFile("/home/yuzo/NetBeansProjects/ConcurrentProgramming/src/threads/task_01/ex_02/quotes.txt");
             System.out.println(fileData);
             try {
                 Thread.sleep(10000);
